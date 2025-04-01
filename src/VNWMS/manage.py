6c5ee88604f5d3446b128eeff3ea6f53f97d1f3e
Box Chang <forkome@gmail.com>
2025-04-01 16:58:18 +0700

    Update

--- src/VNWMS/manage.py
+++ src/VNWMS/manage.py
@@ -6,7 +6,7 @@ from VNWMS.settings.base import DEBUG
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'VNWMS.settings.test')
+                          'VNWMS.settings.production')
 
     # from django.conf import settings
     # if DEBUG:
